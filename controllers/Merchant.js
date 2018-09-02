e9c17e95def6f90a474ea68bbfd1f5bbd528aa7d
ruannelloyd <ruannelloyd@gmail.com>
2018-09-02 10:15:06 +0000

    Generated code by SwaggerHub

--- controllers/Merchant.js
+++ controllers/Merchant.js
@@ -3,6 +3,16 @@
 var utils = require('../utils/writer.js');
 var Merchant = require('../service/MerchantService');
 
+module.exports.getAccount = function getAccount (req, res, next) {
+  Merchant.getAccount()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
 module.exports.newAccount = function newAccount (req, res, next) {
   Merchant.newAccount()
     .then(function (response) {
